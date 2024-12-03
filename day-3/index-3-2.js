d2472b9db6724fb5e15a5db60e1852f584b8fb4e
James Pickard <james.pickard@gmail.com>
2024-12-03 22:48:40 +0000

    Remove logging

--- day-3/index-3-2.js
+++ day-3/index-3-2.js
@@ -4,21 +4,15 @@ const matches = input.matchAll(
   /(?<m>mul\(\d{1,3},\d{1,3}\))|(?<disable>don't\(\))|(?<enable>do\(\))/g,
 );
 
-// console.dir(matches);
-
 let isEnabled = true;
 let total = 0;
 for (const match of matches) {
-  console.dir(match.groups);
-
   if (match.groups.enable) {
-    console.log("Enabling...");
     isEnabled = true;
     continue;
   }
 
   if (match.groups.disable) {
-    console.log("Disabling...");
     isEnabled = false;
     continue;
   }
@@ -27,7 +21,6 @@ for (const match of matches) {
     continue;
   }
 
-  console.log("Multiplying...");
   const [a, b] = match.groups.m.match(/\d{1,3}/g).map((n) => parseInt(n));
   total += a * b;
 }
